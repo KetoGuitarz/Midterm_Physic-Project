b347db8c03a5423feef993cc3be502934437fd17
rattikan-mata <rattikan.mata@bumail.net>
2026-03-27 23:53:13 +0700

    Pendulum and Dog

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform c = GetComponent<Transform>();
+        c.transform.Translate(speed * Time.deltaTime * Vector3.forward);
+    }
+}
