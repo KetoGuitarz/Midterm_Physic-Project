fbd8e8376125d19d46541ebb1c9632141a7c4964
rattikan-mata <rattikan.mata@bumail.net>
2026-03-28 00:37:26 +0700

    CameraV3

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -2,22 +2,18 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    public Transform target;
+    public GameObject player;
     public Vector3 offset;
     public float smoothSpeed = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 rotatedOffset = player.transform.rotation * offset;
+
+        Vector3 desiredPosition = player.transform.position + rotatedOffset;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
 
-        Quaternion desiredRotation = Quaternion.LookRotation(target.position - transform.position);
+        Quaternion desiredRotation = player.transform.rotation;
         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed * Time.deltaTime);
     }
 }
